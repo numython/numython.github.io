0fe8a9376fd5d0268e7a2d1b32726bed068c50a2
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-10-17 21:14:44 -0500

    Nikola auto commit.
    Nikola version: 7.8.0

--- cache/.mako.tmp/list_post.tmpl.py
+++ cache/.mako.tmp/list_post.tmpl.py
@@ -5,10 +5,10 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1476756402.363
+_modified_time = 1476756875.829
 _enable_loop = True
 _template_filename = u'c:/users/adminis/documents/blog/nikolab/lib/site-packages/nikola/data/themes/base/templates/list_post.tmpl'
-_template_uri = 'list_post.tmpl'
+_template_uri = u'list_post.tmpl'
 _source_encoding = 'utf-8'
 _exports = [u'content']
 
