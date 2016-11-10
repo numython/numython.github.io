7722b992560530ceb621df87b0050c74973421eb
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-11-10 07:22:54 -0600

    Nikola auto commit.
    Nikola version: 7.8.0

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Pedro Jorge De Los Santos"  # (translatable)
-BLOG_TITLE = "Numython"  # (translatable)
+BLOG_TITLE = "Numython: Python para ingenieros"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://numython.github.io/"
