14efafbbb07602f30694705cc3d2724a5c7f401b
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2018-04-04 20:24:10 -0500

    Nikola auto commit.
    Nikola version: 7.8.10

--- cache/.mako.tmp/index_helper.tmpl.py
+++ cache/.mako.tmp/index_helper.tmpl.py
@@ -5,45 +5,32 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1489545149.86
+_modified_time = 1522891419.5771966
 _enable_loop = True
-_template_filename = u'c:/users/adminis/documents/blog/nikolab/lib/site-packages/nikola/data/themes/base/templates/index_helper.tmpl'
-_template_uri = u'index_helper.tmpl'
+_template_filename = 'c:/users/user/documents/blog/nikola/lib/site-packages/nikola/data/themes/base/templates/index_helper.tmpl'
+_template_uri = 'index_helper.tmpl'
 _source_encoding = 'utf-8'
-_exports = ['mathjax_script', 'html_pager']
+_exports = ['html_pager', 'mathjax_script']
 
+
+def _mako_get_namespace(context, name):
+    try:
+        return context.namespaces[(__name__, name)]
+    except KeyError:
+        _mako_generate_namespaces(context)
+        return context.namespaces[(__name__, name)]
+def _mako_generate_namespaces(context):
+    ns = runtime.TemplateNamespace('math', context._clean_inheritance_tokens(), templateuri='math_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'math')] = ns
 
 def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         __M_writer = context.writer()
-        __M_writer(u'\n\n')
-        __M_writer(u'\n')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_mathjax_script(context,posts):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        mathjax_config = context.get('mathjax_config', UNDEFINED)
-        any = context.get('any', UNDEFINED)
-        use_katex = context.get('use_katex', UNDEFINED)
-        __M_writer = context.writer()
-        __M_writer(u'\n')
-        if any(post.is_mathjax for post in posts):
-            if use_katex:
-                __M_writer(u'            <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.js"></script>\n            <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/contrib/auto-render.min.js"></script>\n            <script>\n                renderMathInElement(document.body);\n            </script>\n')
-            else:
-                __M_writer(u'            <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"> </script>\n')
-                if mathjax_config:
-                    __M_writer(u'            ')
-                    __M_writer(unicode(mathjax_config))
-                    __M_writer(u'\n')
-                else:
-                    __M_writer(u'            <script type="text/x-mathjax-config">\n            MathJax.Hub.Config({tex2jax: {inlineMath: [[\'$latex \',\'$\'], [\'\\\\(\',\'\\\\)\']]}});\n            </script>\n')
+        __M_writer('\n')
+        __M_writer('\n\n')
+        __M_writer('\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -52,26 +39,39 @@ def render_mathjax_script(context,posts):
 def render_html_pager(context):
     __M_caller = context.caller_stack._push_frame()
     try:
+        nextlink = context.get('nextlink', UNDEFINED)
         prevlink = context.get('prevlink', UNDEFINED)
         messages = context.get('messages', UNDEFINED)
-        nextlink = context.get('nextlink', UNDEFINED)
         __M_writer = context.writer()
-        __M_writer(u'\n')
+        __M_writer('\n')
         if prevlink or nextlink:
-            __M_writer(u'        <nav class="postindexpager">\n        <ul class="pager">\n')
+            __M_writer('        <nav class="postindexpager">\n        <ul class="pager">\n')
             if prevlink:
-                __M_writer(u'            <li class="previous">\n                <a href="')
-                __M_writer(unicode(prevlink))
-                __M_writer(u'" rel="prev">')
-                __M_writer(unicode(messages("Newer posts")))
-                __M_writer(u'</a>\n            </li>\n')
+                __M_writer('            <li class="previous">\n                <a href="')
+                __M_writer(str(prevlink))
+                __M_writer('" rel="prev">')
+                __M_writer(str(messages("Newer posts")))
+                __M_writer('</a>\n            </li>\n')
             if nextlink:
-                __M_writer(u'            <li class="next">\n                <a href="')
-                __M_writer(unicode(nextlink))
-                __M_writer(u'" rel="next">')
-                __M_writer(unicode(messages("Older posts")))
-                __M_writer(u'</a>\n            </li>\n')
-            __M_writer(u'        </ul>\n        </nav>\n')
+                __M_writer('            <li class="next">\n                <a href="')
+                __M_writer(str(nextlink))
+                __M_writer('" rel="next">')
+                __M_writer(str(messages("Older posts")))
+                __M_writer('</a>\n            </li>\n')
+            __M_writer('        </ul>\n        </nav>\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_mathjax_script(context,posts):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        math = _mako_get_namespace(context, 'math')
+        __M_writer = context.writer()
+        __M_writer('\n    ')
+        __M_writer(str(math.math_scripts_ifposts(posts)))
+        __M_writer('\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -79,6 +79,6 @@ def render_html_pager(context):
 
 """
 __M_BEGIN_METADATA
-{"source_encoding": "utf-8", "line_map": {"16": 0, "21": 19, "22": 40, "28": 21, "35": 21, "36": 22, "37": 23, "38": 24, "39": 29, "40": 30, "41": 31, "42": 32, "43": 32, "44": 32, "45": 33, "46": 34, "52": 2, "59": 2, "60": 3, "61": 4, "62": 6, "63": 7, "64": 8, "65": 8, "66": 8, "67": 8, "68": 11, "69": 12, "70": 13, "71": 13, "72": 13, "73": 13, "74": 16, "80": 74}, "uri": "index_helper.tmpl", "filename": "c:/users/adminis/documents/blog/nikolab/lib/site-packages/nikola/data/themes/base/templates/index_helper.tmpl"}
+{"filename": "c:/users/user/documents/blog/nikola/lib/site-packages/nikola/data/themes/base/templates/index_helper.tmpl", "uri": "index_helper.tmpl", "source_encoding": "utf-8", "line_map": {"23": 2, "26": 0, "31": 2, "32": 20, "33": 25, "39": 3, "46": 3, "47": 4, "48": 5, "49": 7, "50": 8, "51": 9, "52": 9, "53": 9, "54": 9, "55": 12, "56": 13, "57": 14, "58": 14, "59": 14, "60": 14, "61": 17, "67": 23, "72": 23, "73": 24, "74": 24, "80": 74}}
 __M_END_METADATA
 """
--- cache/.mako.tmp/list.tmpl.py
+++ cache/.mako.tmp/list.tmpl.py
@@ -5,12 +5,12 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1489545149.65
+_modified_time = 1522891419.4751909
 _enable_loop = True
-_template_filename = u'c:/users/adminis/documents/blog/nikolab/lib/site-packages/nikola/data/themes/base/templates/list.tmpl'
+_template_filename = 'c:/users/user/documents/blog/nikola/lib/site-packages/nikola/data/themes/base/templates/list.tmpl'
 _template_uri = 'list.tmpl'
 _source_encoding = 'utf-8'
-_exports = [u'content']
+_exports = ['content']
 
 
 def _mako_get_namespace(context, name):
@@ -20,26 +20,38 @@ def _mako_get_namespace(context, name):
         _mako_generate_namespaces(context)
         return context.namespaces[(__name__, name)]
 def _mako_generate_namespaces(context):
-    pass
+    ns = runtime.TemplateNamespace('archive_nav', context._clean_inheritance_tokens(), templateuri='archive_navigation_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'archive_nav')] = ns
+
+    ns = runtime.TemplateNamespace('feeds_translations', context._clean_inheritance_tokens(), templateuri='feeds_translations_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'feeds_translations')] = ns
+
 def _mako_inherit(template, context):
     _mako_generate_namespaces(context)
-    return runtime._inherit_from(context, u'base.tmpl', _template_uri)
+    return runtime._inherit_from(context, 'base.tmpl', _template_uri)
 def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
+        _import_ns = {}
+        _mako_get_namespace(context, 'archive_nav')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
+        items = _import_ns.get('items', context.get('items', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
         def content():
             return render_content(context._locals(__M_locals))
-        items = context.get('items', UNDEFINED)
-        messages = context.get('messages', UNDEFINED)
-        title = context.get('title', UNDEFINED)
+        archive_nav = _mako_get_namespace(context, 'archive_nav')
+        title = _import_ns.get('title', context.get('title', UNDEFINED))
         __M_writer = context.writer()
-        __M_writer(u'\n\n')
+        __M_writer('\n')
+        __M_writer('\n')
+        __M_writer('\n\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
             context['self'].content(**pageargs)
         
 
-        __M_writer(u'\n')
+        __M_writer('\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -48,33 +60,42 @@ def render_body(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'archive_nav')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
+        items = _import_ns.get('items', context.get('items', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
         def content():
             return render_content(context)
-        items = context.get('items', UNDEFINED)
-        messages = context.get('messages', UNDEFINED)
-        title = context.get('title', UNDEFINED)
+        archive_nav = _mako_get_namespace(context, 'archive_nav')
+        title = _import_ns.get('title', context.get('title', UNDEFINED))
         __M_writer = context.writer()
-        __M_writer(u'\n<article class="listpage">\n    <header>\n        <h1>')
-        __M_writer(filters.html_escape(unicode(title)))
-        __M_writer(u'</h1>\n    </header>\n')
+        __M_writer('\n<article class="listpage">\n    <header>\n        <h1>')
+        __M_writer(filters.html_escape(str(title)))
+        __M_writer('</h1>\n    </header>\n    ')
+        __M_writer(str(archive_nav.archive_navigation()))
+        __M_writer('\n    ')
+        __M_writer(str(feeds_translations.translation_link()))
+        __M_writer('\n')
         if items:
-            __M_writer(u'    <ul class="postlist">\n')
+            __M_writer('    <ul class="postlist">\n')
             for text, link, count in items:
-                __M_writer(u'        <li><a href="')
-                __M_writer(unicode(link))
-                __M_writer(u'">')
-                __M_writer(filters.html_escape(unicode(text)))
-                __M_writer(u'</a>\n')
+                __M_writer('        <li><a href="')
+                __M_writer(str(link))
+                __M_writer('">')
+                __M_writer(filters.html_escape(str(text)))
+                __M_writer('</a>\n')
                 if count:
-                    __M_writer(u'            (')
-                    __M_writer(unicode(count))
-                    __M_writer(u')\n')
-            __M_writer(u'    </ul>\n')
+                    __M_writer('            (')
+                    __M_writer(str(count))
+                    __M_writer(')\n')
+            __M_writer('    </ul>\n')
         else:
-            __M_writer(u'    <p>')
-            __M_writer(unicode(messages("Nothing found.")))
-            __M_writer(u'</p>\n')
-        __M_writer(u'</article>\n')
+            __M_writer('    <p>')
+            __M_writer(str(messages("Nothing found.")))
+            __M_writer('</p>\n')
+        __M_writer('</article>\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -82,6 +103,6 @@ def render_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"source_encoding": "utf-8", "line_map": {"27": 0, "37": 2, "42": 22, "48": 4, "57": 4, "58": 7, "59": 7, "60": 9, "61": 10, "62": 11, "63": 12, "64": 12, "65": 12, "66": 12, "67": 12, "68": 13, "69": 14, "70": 14, "71": 14, "72": 17, "73": 18, "74": 19, "75": 19, "76": 19, "77": 21, "83": 77}, "uri": "list.tmpl", "filename": "c:/users/adminis/documents/blog/nikolab/lib/site-packages/nikola/data/themes/base/templates/list.tmpl"}
+{"filename": "c:/users/user/documents/blog/nikola/lib/site-packages/nikola/data/themes/base/templates/list.tmpl", "uri": "list.tmpl", "source_encoding": "utf-8", "line_map": {"23": 3, "26": 4, "32": 0, "47": 2, "48": 3, "49": 4, "54": 26, "60": 6, "74": 6, "75": 9, "76": 9, "77": 11, "78": 11, "79": 12, "80": 12, "81": 13, "82": 14, "83": 15, "84": 16, "85": 16, "86": 16, "87": 16, "88": 16, "89": 17, "90": 18, "91": 18, "92": 18, "93": 21, "94": 22, "95": 23, "96": 23, "97": 23, "98": 25, "104": 98}}
 __M_END_METADATA
 """
