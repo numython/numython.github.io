fc4586bac1d9c74e66d4d512f74a61689b2fcf99
JorgeDeLosSantos <delossantosmfq@gmail.com>
2018-12-12 00:18:23 -0600

    Nikola auto commit.
    Nikola version: 8.0.0

--- conf.py
+++ conf.py
@@ -140,7 +140,8 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+# ~ THEME = "bootstrap3"
+THEME = "material-theme"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
